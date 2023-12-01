cceec15bb6782718a5a950041683fee35b70279f
Daniel Bucket <danieljbucket@gmail.com>
2023-12-01 10:00:10 -0700

    end Loading Data section

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import {
+  Outlet,
+  Link,
+  useLoaderData,
+} from "react-router-dom";
+import { getContacts } from '../contacts';
+
+export async function loader() {
+	const contacts = await getContacts();
+	return { contacts };
+};
 
 export default function Root() {
+	const { contacts } = useLoaderData();
   return (
     <>
       <div id="sidebar">
@@ -28,15 +39,29 @@ export default function Root() {
             <button type="submit">New</button>
           </form>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
+         <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="detail">
