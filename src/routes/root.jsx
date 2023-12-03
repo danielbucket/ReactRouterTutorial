e908bedb89b08322a385f8c522cba44e35ca113b
Daniel Bucket <danieljbucket@gmail.com>
2023-12-03 12:26:00 -0700

    end "managing the history stack" section

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,7 +6,9 @@ import {
   Form,
   redirect,
   useNavigation,
+  useSubmit,
 } from "react-router-dom";
+import { useEffect } from 'react';
 import { getContacts, createContact } from '../contacts';
 
 export async function action() {
@@ -14,38 +16,53 @@ export async function action() {
 	return redirect(`/contacts/${contact.id}/edit`);
 };
 
-export async function loader() {
-	const contacts = await getContacts();
-	return { contacts };
+export async function loader({ request }) {
+	const url = new URL(request.url);
+	const q = url.searchParams.get("g");
+	const contacts = await getContacts(q);
+	return { contacts, q };
 };
 
 export default function Root() {
-	const { contacts } = useLoaderData();
+	const { contacts, q } = useLoaderData();
 	const navigation = useNavigation();
+	const submit = useSubmit();
+	const searching =
+		navigation.location && 
+		new URLSearchParams(navigation.location.search).has("q");
+
+	useEffect(() => {
+		document.getElementById("q").value = q;
+	}, [q]);
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
+              onChange={(event)=> {
+              	submit(event.currentTarget.form);
+              }}
             />
             <div
               id="search-spinner"
               aria-hidden
-              hidden={true}
+              hidden={!searching}
             />
             <div
               className="sr-only"
               aria-live="polite"
             ></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
